56418d8a23a5a649c247e827314810ba36905851
leilei405 <1601178425@qq.com>
2025-07-05 22:18:22 +0800

    feat: ✨taro 初体验 完成基本使用 写个小页面 组件使用  语法熟悉

---
+++ src/pages/index/constant.ts
@@ -0,0 +1,22 @@
+export const DEFAULT_TAB_LIST = [
+  {
+    title: "机票",
+    tab: "flight",
+    index: 0,
+  },
+  {
+    title: "火车票",
+    tab: "train",
+    index: 1,
+  },
+  {
+    title: "酒店",
+    tab: "hotel",
+    index: 2,
+  },
+  {
+    title: "汽车票",
+    tab: "bus",
+    index: 3,
+  },
+];
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,46 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { useState } from "react";
+import { View } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import { DEFAULT_TAB_LIST } from "./constant";
+import "./index.scss";
 
-export default function Index () {
+export default function Index() {
+  const [activeTab, setActiveTab] = useState(0);
+  const innerStyle = {
+    width: `${100 / DEFAULT_TAB_LIST.length}%`,
+    transform: `translateX(${activeTab * 100}%)`,
+  };
+
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  const onChangeTab = (tab) => {
+    console.log(`Tab changed to: ${tab}`);
+    setActiveTab(tab);
+  };
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
+    <View className="index-container">
+      <View className="top">
+        <View className="tab-list">
+          {DEFAULT_TAB_LIST.map((item) => (
+            <View
+              className={`tab-item ${activeTab === item.index ? "active" : ""}`}
+              key={item.index}
+              onClick={() => onChangeTab(item.index)}
+            >
+              {item.title}
+            </View>
+          ))}
+        </View>
+        <View className="scrollbar" style={innerStyle} />
+      </View>
+      {DEFAULT_TAB_LIST[activeTab]["tab"] === "flight" ? (
+        <View className="content">111</View>
+      ) : (
+        <View className="content">222</View>
+      )}
     </View>
-  )
+  );
 }
